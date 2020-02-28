09746157f5cb765655f1e7e1ce18f6e79cd7c448
Raven-Witch <54775307+Raven-Witch@users.noreply.github.com>
2020-02-28 14:15:57 +0100

    Hinzugefügt weitere Parameter für SubjektDaten und ausgabe in BedingungenAnzeigen.aktualisieren

--- src/enteAufgabe/ObserverPattern/SubjektDaten.java
+++ src/enteAufgabe/ObserverPattern/SubjektDaten.java
@@ -2,8 +2,14 @@ package enteAufgabe.ObserverPattern;
 
 public class SubjektDaten {
     private int i;
+    private String t;
 
     public int getI() {
         return i;
     }
+
+    public String getT() {
+        return t;
+    }
+
 }
